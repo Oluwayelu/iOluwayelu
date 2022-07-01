ceea35a246d9b65c15ceeeabcb5c6e72973a3417
oluwayelu <oluwayeluifeoluwa@gmail.com>
2022-07-01 20:39:24 +0100

    fix: remove theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-// import { useSmoothScroll } from "hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // useSmoothScroll();
-  return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./sections/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
